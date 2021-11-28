66ad4c3b68b77034406d5fb0a6767d19bc3d49e7
SebastianKennedy <sebastiankennedy@foxmail.com>
2021-11-28 22:17:52 +0800

    Added ch1.

--- ch1/server_1.go
+++ ch1/server_1.go
@@ -7,11 +7,10 @@ import (
 )
 
 func main() {
-    http.HandleFunc("/", handler) // each request calls handler
+    http.HandleFunc("/", handler)
     log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-// handler echoes the Path component of the request URL r.
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
--- ch1/server_2.go
+++ ch1/server_2.go
@@ -16,7 +16,6 @@ func main() {
     log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-// handler echoes the Path component of the requested URL.
 func handler(w http.ResponseWriter, r *http.Request) {
     mu.Lock()
     count++
@@ -24,7 +23,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
-// counter echoes the number of calls so far.
 func counter(w http.ResponseWriter, r *http.Request) {
     mu.Lock()
     fmt.Fprintf(w, "Count %d\n", count)
